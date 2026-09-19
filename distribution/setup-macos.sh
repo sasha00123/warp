7afc5843466fc5fe79ada08a5ff8b593e348d844
Aleksandr Batyrgariev <alexander.batyrgariev@yandex.ru>
2026-09-19 22:27:24 +0400

    Install the upstream third-party license generator

--- distribution/setup-macos.sh
+++ distribution/setup-macos.sh
@@ -17,6 +17,7 @@ if [[ -d app ]]; then
   fi
   # Install from the public registry, never a precompiled upstream-internal cache.
   cargo install cargo-bundle --version 0.11.0 --locked
+  cargo install cargo-about --version 0.8.4 --locked
 else
   cargo install cargo-bundle --git https://github.com/zed-industries/cargo-bundle.git --rev 2be2669972dff3ddd4daf89a2cb29d2d06cad7c7 --locked
 fi
